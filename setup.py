da396be230a90edb0ebd1a44a9df4ae46790e089
semen603089 <semen603089@mail.ru>
2022-11-26 23:57:59 +0300

    setup init

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt", "r") as req_file:
+    req = req_file.read().splitlines()
+
+
+setup(
+    name="auth_lub",
+    version="0.0.1",
+    author="Semyon Grigoriev",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/profcomff/auth-lib",
+    packages=find_packages(),
+    install_requires=req,
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
