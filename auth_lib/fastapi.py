14bfdcc8a16adbd4b1c2e64a56b88fbac0c33119
semen603089 <semen603089@mail.ru>
2022-11-30 22:31:07 +0300

    wrapper fix

--- auth_lib/fastapi.py
+++ auth_lib/fastapi.py
@@ -58,7 +58,6 @@ class OAuth2TokenAPI(OAuth2):
 
 def auth_required(url: str):
     def _auth_required(endpoint: Callable[[object], Awaitable[object]]):
-        @wraps(endpoint)
         async def auth_endpoint(*args, request: Request, authorization: str = Header(...), **kwargs) -> object:
             scheme, param = get_authorization_scheme_param(authorization)
             if not authorization or scheme.lower() != "token":
@@ -76,6 +75,21 @@ def auth_required(url: str):
                     headers={"WWW-Authenticate": "Token"},
                 )
             return await endpoint(*args, **kwargs)
+
+        import inspect
+        auth_endpoint.__signature__ = inspect.Signature(
+            parameters=[
+                # Use all parameters from handler
+                *inspect.signature(endpoint).parameters.values(),
+
+                # Skip *args and **kwargs from wrapper parameters:
+                *filter(
+                    lambda p: p.kind not in (inspect.Parameter.VAR_POSITIONAL, inspect.Parameter.VAR_KEYWORD),
+                    inspect.signature(auth_endpoint).parameters.values()
+                )
+            ],
+            return_annotation=inspect.signature(endpoint).return_annotation,
+        )
         return auth_endpoint
     return _auth_required
 
