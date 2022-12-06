1107162754c905b08b47a03da84a12b14935de73
semen603089 <semen603089@mail.ru>
2022-12-06 23:21:56 +0300

    trying to fix

--- auth_lib/fastapi.py
+++ auth_lib/fastapi.py
@@ -4,7 +4,7 @@ import aiohttp
 from fastapi import Depends
 from fastapi.exceptions import HTTPException
 from fastapi.openapi.models import APIKey, APIKeyIn
-from fastapi.security.api_key import APIKeyHeader
+from fastapi.security import APIKeyHeader
 from fastapi.security.base import SecurityBase
 from starlette.requests import Request
 from starlette.status import HTTP_403_FORBIDDEN
