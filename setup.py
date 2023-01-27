eab30d38f5324b0e1337dc08dcc714ef612b878d
semen603089 <semen603089@mail.ru>
2023-01-27 14:02:42 +0300

    review

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-with open("requirements.txt", "r") as req_file:
-    req = req_file.read().splitlines()
-
-with open("requirements.fastapi.txt", "r") as req_file:
-    req_fastapi = req_file.read().splitlines()
-
-
 setup(
     name="auth_lib",
     version="0.0.1",
@@ -18,21 +11,10 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/profcomff/auth-lib",
     packages=find_packages(),
-    install_requires=req,
-    classifiers=[
-        "Programming Language :: Python :: 3.11",
-    ],
-)
-
-setup(
-    name="auth_lib[fastapi]",
-    install_requires=["Package-A[PDF]"].extend(req_fastapi),
-    version="0.0.1",
-    author="Semyon Grigoriev",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    url="https://github.com/profcomff/auth-lib",
-    packages=find_packages(),
+    install_requires=["requests", "aiohttp", "setuptools"],
+    extras_require={
+        "fastapi": ["fastapi", "starlette"],
+    },
     classifiers=[
         "Programming Language :: Python :: 3.11",
     ],
