7dbbc2837744b61bb1c09f4709666b6c617d31f2
Artem Morozov <morozoff234@yandex.ru>
2023-07-29 22:53:45 +0300

    Added userdata in user information. Fixes after review.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as readme_file:
+with open("README.md", "r", encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 setup(
