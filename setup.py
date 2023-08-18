07ba0c818598286c9b5c5d83e5f0bf181a63561e
Roman Dyakov <ROMAN@DYAKOV.SPACE>
2023-08-19 00:51:57 +0300

    Fix requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
+
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    req = req_file.read().split('\n')
+
+
 setup(
     name="auth_lib_profcomff",
     version="2023.08.06",
@@ -11,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/profcomff/auth-lib",
     packages=find_packages(),
-    install_requires=["requests", "aiohttp", "setuptools"],
+    install_requires=req,
     extras_require={
         "fastapi": ["fastapi", "starlette", "pydantic", "pydantic_settings"],
         "testing": ["pytest"],
